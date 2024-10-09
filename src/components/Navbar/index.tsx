bfeb751955231341c4c2c1fedc0ae22039b18e3b
nurlan <baitassovnurlan1507@gmail.com>
2024-10-10 01:22:17 +0500

    feat: add Header

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header className="bg-primary w-full p-4">
+            <nav className="flex justify-between items-center">
+                <div className="flex space-x-5 basis-1/2">
+                    <span className="text-white font-dancing text-3xl">BeyondTech</span>
+                </div>
+
+                {/* <input id="people_search" className="px-2 py-1 rounded" type="text" placeholder="Search..." /> */}
+
+                <ul className="flex space-x-4">
+                    <li>
+                        <a href="#" className="text-white">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-white">
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="text-white">
+                            Contact
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
